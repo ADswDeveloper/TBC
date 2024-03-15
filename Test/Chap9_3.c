fa417edbcabfed4f3fbe3dbc8074f511526dea48
jinho2.jeong <wjd526999@gmail.com>
2024-03-15 20:41:44 +0900

    first commit

---
+++ Test/Chap9_3.c
@@ -0,0 +1,18 @@
+// Chap _3.cpp : 
+// 강의 요약 
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+
+int main()
+{
+    char a[] = "abc";
+    int b[] = { 1,2,3 };
+    // Git Commit test....
+    // stash test
+    // commit test
+    return 0;
+}
+
